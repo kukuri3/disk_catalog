13482fb67ebc5b5077f2f084d9ec3fb8aca2fff1
mima <kukuri3@gmail.com>
2015-09-08 11:45:22 +0900

    first commit

---
+++ disk_catalog/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.driveListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DriveListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.volume = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.wasuu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ext = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullpath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.list_dup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fn2_dup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 204);
+            this.textBox1.TabIndex = 1;
+            // 
+            // driveListBox1
+            // 
+            this.driveListBox1.FormattingEnabled = true;
+            this.driveListBox1.Location = new System.Drawing.Point(14, 12);
+            this.driveListBox1.Name = "driveListBox1";
+            this.driveListBox1.Size = new System.Drawing.Size(121, 20);
+            this.driveListBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ドライブ情報取得";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.no,
+            this.volume,
+            this.fn,
+            this.fn2,
+            this.wasuu,
+            this.ext,
+            this.date,
+            this.size,
+            this.fullpath,
+            this.list_dup,
+            this.fn2_dup});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 327);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(915, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // no
+            // 
+            this.no.HeaderText = "no";
+            this.no.Name = "no";
+            this.no.Width = 50;
+            // 
+            // volume
+            // 
+            this.volume.HeaderText = "volume";
+            this.volume.Name = "volume";
+            this.volume.Width = 80;
+            // 
+            // fn
+            // 
+            this.fn.HeaderText = "fn";
+            this.fn.Name = "fn";
+            // 
+            // fn2
+            // 
+            this.fn2.HeaderText = "fn2";
+            this.fn2.Name = "fn2";
+            // 
+            // wasuu
+            // 
+            this.wasuu.HeaderText = "#";
+            this.wasuu.Name = "wasuu";
+            this.wasuu.Width = 50;
+            // 
+            // ext
+            // 
+            this.ext.HeaderText = "ext";
+            this.ext.Name = "ext";
+            this.ext.Width = 50;
+            // 
+            // date
+            // 
+            this.date.HeaderText = "date";
+            this.date.Name = "date";
+            // 
+            // size
+            // 
+            this.size.HeaderText = "size";
+            this.size.Name = "size";
+            this.size.Width = 80;
+            // 
+            // fullpath
+            // 
+            this.fullpath.HeaderText = "fullpath";
+            this.fullpath.Name = "fullpath";
+            // 
+            // list_dup
+            // 
+            this.list_dup.HeaderText = "list_dup";
+            this.list_dup.Name = "list_dup";
+            this.list_dup.Width = 50;
+            // 
+            // fn2_dup
+            // 
+            this.fn2_dup.HeaderText = "fn2_dup";
+            this.fn2_dup.Name = "fn2_dup";
+            this.fn2_dup.Width = 50;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 503);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(885, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(141, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(205, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "list_dupcheck";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(286, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "delete dup";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 544);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.driveListBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.DriveListBox driveListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn volume;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn wasuu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ext;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullpath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn list_dup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fn2_dup;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ disk_catalog/Form1.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+
+
+//using System.IO;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string gDrive;
+        string gSerial;
+        int gLabelcount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 選択モードを行単位での選択のみにする
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void xGetDriveInfo()
+        {
+                        //ドライブの情報を取得する
+            
+            gDrive = driveListBox1.SelectedItem.ToString().Substring(0,2);  //ドライブ文字を取得
+            
+            System.Management.ManagementObject mo =
+            new System.Management.ManagementObject("Win32_LogicalDisk=\"" + gDrive + "\"");
+
+
+            //名前（"C:"など）
+            textBox1.AppendText("名前:{0}" +
+                (string)mo.Properties["Name"].Value + "\r\n");
+            //ドライブタイプ
+            string typeDescription = "";
+            switch ((uint)mo.Properties["DriveType"].Value)
+            {
+                case 5:
+                    typeDescription = "光ディスクドライブ(CD-ROM,DVD-ROMなど)";
+                    break;
+                case 3:
+                    typeDescription = "固定ディスク";
+                    break;
+                case 4:
+                    typeDescription = "ネットワークドライブ";
+                    break;
+                case 1:
+                    typeDescription = "ルートディレクトリがない";
+                    break;
+                case 6:
+                    typeDescription = "RAMディスク";
+                    break;
+                case 2:
+                    typeDescription = "リムーバブルストレージデバイス" +
+                        "フロッピーディスクドライブ、USBフラッシュドライブなど";
+                    break;
+                case 0:
+                    typeDescription = "不明";
+                    break;
+            }
+            textBox1.AppendText("ドライブタイプ:{0}" + typeDescription + "\n");
+            //ボリュームラベル
+            textBox1.AppendText("ボリュームラベル:{0}" +
+                (string)mo.Properties["VolumeName"].Value + "\n");
+            //ボリュームシリアルナンバー
+            textBox1.AppendText("ボリュームシリアルナンバー:{0}" +
+                (string)mo.Properties["VolumeSerialNumber"].Value + "\n");
+            gSerial = (string)mo.Properties["VolumeSerialNumber"].Value;
+            //ファイルシステム(NTFS、FAT32など)
+            textBox1.AppendText("ファイルシステム:{0}" +
+                (string)mo.Properties["FileSystem"].Value + "\n");
+            //説明（「ローカル固定ディスク」など）
+            textBox1.AppendText("説明:{0}" +
+                (string)mo.Properties["Description"].Value + "\n");
+
+            //ドライブの容量を取得する
+            ulong ts = (ulong)mo.Properties["Size"].Value;
+            textBox1.AppendText("サイズGB" + ts / 1000000000 + "\n");
+            //C:のドライブの空き容量を取得する
+            ulong fs = (ulong)mo.Properties["FreeSpace"].Value;
+            textBox1.AppendText("空き容量GB" + fs / 1000000000 + "\n");
+            mo.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //ドライブ情報を取得
+            xGetDriveInfo();
+
+            //ドライブ内のファイル情報を取得
+            string[] stFilePathes;
+            string ss = "";
+            int i = 0;
+
+            DateTime now;
+            TimeSpan elapsed;
+
+            now = DateTime.Now;
+            textBox1.AppendText("start time=" + now+"\n");
+            stFilePathes = GetFilesMostDeep(gDrive + "\\", "*.*");  //探索する
+            elapsed = DateTime.Now - now;
+            textBox1.AppendText("elapsed="+elapsed+"\n");
+            int num = stFilePathes.Length;
+
+            
+            textBox1.AppendText("num=" + num + "\n");
+            textBox1.Refresh();
+            // 取得したファイル名を列挙する
+            label1.Text = "登録中..";
+            int j = 0;
+            foreach (string stFilePath in stFilePathes)
+            {
+                string ext = System.IO.Path.GetExtension(stFilePath);   //ext
+                string fn = System.IO.Path.GetFileName(stFilePath);   //fn
+                string fn2 = System.IO.Path.GetFileName(stFilePath);   //fn2
+                string fullpath = System.IO.Path.GetFullPath(stFilePath).Substring(2); //fullpathドライブ名を除いたパス
+                DateTime dtUpdate = System.IO.File.GetLastWriteTime(stFilePath);    //date
+                System.IO.FileInfo fi = new System.IO.FileInfo(stFilePath);
+                long filesize = fi.Length;
+                int wasuu = 0;
+
+
+//                if (true)
+                    if (ext.Equals(".txt"))
+                    {
+  
+                    //datagridviewに追加。
+                    //no,volume,name,ext,date,size,fullpath
+                        dataGridView1.Rows.Add(i, gSerial, fn, fn2, wasuu,ext, dtUpdate, filesize, fullpath, "-", "-");
+                    i++;
+                    
+                }
+                if ((j % 100) == 0)
+                {
+                    progressBar1.Value = j * 100 / num;
+                    progressBar1.Refresh();
+                    // メッセージ・キューにあるWindowsメッセージをすべて処理する(応答なしの回避)
+                    Application.DoEvents();
+                }
+                j++;
+
+            }
+            textBox1.AppendText("total " + i + "\n");
+            textBox1.AppendText("linenum=" + dataGridView1.RowCount+"\n");
+
+        }
+        /// ---------------------------------------------------------------------------------------
+        /// <summary>
+        ///     指定した検索パターンに一致するファイルを最下層まで検索しすべて返します。</summary>
+        /// <param name="stRootPath">
+        ///     検索を開始する最上層のディレクトリへのパス。</param>
+        /// <param name="stPattern">
+        ///     パス内のファイル名と対応させる検索文字列。</param>
+        /// <returns>
+        ///     検索パターンに一致したすべてのファイルパス。</returns>
+        /// ---------------------------------------------------------------------------------------
+        public string[] GetFilesMostDeep(string stRootPath, string stPattern)
+        {
+            System.Collections.Specialized.StringCollection hStringCollection = (
+                new System.Collections.Specialized.StringCollection()
+            );
+
+            // このディレクトリ内のすべてのファイルを検索する
+            try
+            {
+                foreach (string stFilePath in System.IO.Directory.GetFiles(stRootPath, stPattern))
+                {
+
+                    hStringCollection.Add(stFilePath);
+                    if ((gLabelcount % 100) == 0)  //時々表示
+                    {
+                        label1.Text = "探索中.."+stFilePath;
+                        label1.Refresh();
+                        // メッセージ・キューにあるWindowsメッセージをすべて処理する(応答なしの回避)
+                        Application.DoEvents();
+                    }
+                    gLabelcount++;
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+
+            // このディレクトリ内のすべてのサブディレクトリを検索する (再帰)
+            try
+            {
+                foreach (string stDirPath in System.IO.Directory.GetDirectories(stRootPath))
+                {
+                    string[] stFilePathes = GetFilesMostDeep(stDirPath, stPattern);
+
+                    // 条件に合致したファイルがあった場合は、ArrayList に加える
+                    if (stFilePathes != null)
+                    {
+                        hStringCollection.AddRange(stFilePathes);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            // StringCollection を 1 次元の String 配列にして返す
+            string[] stReturns = new string[hStringCollection.Count];
+            hStringCollection.CopyTo(stReturns, 0);
+
+            return stReturns;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // 保存用のファイルを開く(true=追記モード)
+            using (StreamWriter writer = new StreamWriter("test2.csv", true, Encoding.GetEncoding("shift_jis")))
+            {
+
+                int rowCount = dataGridView1.Rows.Count;
+
+                xLog("保存中...");
+
+                // ユーザによる行追加が許可されている場合は、最後に新規入力用の
+                // 1行分を差し引く
+                if (dataGridView1.AllowUserToAddRows == true)
+                {
+                    rowCount = rowCount - 1;
+                }
+
+                // 行
+                for (int i = 0; i < rowCount; i++)
+                {
+                    // リストの初期化
+                    List<String> strList = new List<String>();
+
+                    // 列
+                    String strCsvData = "";
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        strCsvData+="\""+(dataGridView1[j, i].Value.ToString())+"\""+",";
+                    }
+ 
+                    writer.WriteLine(strCsvData);
+                    progressBar1.Value = ((i+1)*100) / rowCount;
+                    progressBar1.Update();
+                }
+                xLog("終了。\r\n");
+
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //csvファイルのload
+
+            
+            TextFieldParser parser = new TextFieldParser("test2.csv", Encoding.GetEncoding("Shift_JIS"));
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(","); // 区切り文字はコンマ
+
+            string[] setLines;
+
+            //予め行数を取得
+            string fileName = "test2.csv";
+            System.Text.Encoding enc = System.Text.Encoding.GetEncoding("shift_jis");
+
+            setLines = System.IO.File.ReadAllLines(fileName, enc);
+
+            int lines = setLines.Length;
+            xLog("lines=" + lines);
+
+
+
+            // データをすべてクリア
+            dataGridView1.Rows.Clear();
+
+            int linenum = 0;
+
+            while (!parser.EndOfData)
+            {
+                string[] row = parser.ReadFields(); // 1行読み込み
+                // 読み込んだデータ(1行をDataGridViewに表示する)
+                dataGridView1.Rows.Add(row);
+                linenum++;
+                progressBar1.Value = ((linenum*100 + 1) / lines);
+                progressBar1.Update();
+                
+            }
+        }
+        private void xDispProcessInfo()
+        {
+            // 自プロセスを取得
+            System.Diagnostics.Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+
+            // リフレッシュしないとプロセスの各種情報が最新情報に更新されない
+            currentProcess.Refresh();
+
+            // 各種プロセス情報を出力する
+
+            label2.Text = "メモリ使用量=" + currentProcess.WorkingSet64 / 1000000 + "MB";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            xDispProcessInfo();
+        }
+
+        private void xLog(String s)
+        {
+            //ログ出力
+            DateTime dt = DateTime.Now;
+            textBox1.AppendText(dt.ToString() + " " + s + "\n");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(@"log.txt", true, System.Text.Encoding.GetEncoding("shift_jis"));
+            sw.Write(dt.ToString() + s + "\r\n");
+            sw.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            xDupCheck();
+        }
+        private void xDupCheck()
+        {
+            //dgv内の重複をチェック
+            //行数を取得
+            int rowCount = dataGridView1.Rows.Count;
+            // ユーザによる行追加が許可されている場合は、最後に新規入力用の
+            // 1行分を差し引く
+            if (dataGridView1.AllowUserToAddRows == true)
+            {
+                rowCount = rowCount - 1;
+            }
+
+            int dupcount = 0;
+            for (int i = 0; i < rowCount; i++)
+            {
+                progressBar1.Value = (i*100 + 1) / rowCount;
+                progressBar1.Refresh();
+                for (int j = i+1; j < rowCount; j++)
+                {
+                    string fn0 = dataGridView1["fullpath", i].Value.ToString();    
+                    string fn1 = dataGridView1["fullpath", j].Value.ToString();    
+                    string vol0 = dataGridView1["volume", i].Value.ToString();
+                    string vol1 = dataGridView1["volume", j].Value.ToString();
+                    if ((fn0 == fn1) && (vol0 == vol1))
+                    {
+                        dupcount++;
+                        dataGridView1["list_dup", i].Value = "o";
+                    }
+                }
+            }
+            xLog("重複数="+dupcount);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            xDupDelete();
+        }
+        private void xDupDelete()
+        {
+            //行数を取得
+            int rowCount = dataGridView1.Rows.Count;
+            // ユーザによる行追加が許可されている場合は、最後に新規入力用の
+            // 1行分を差し引く
+            if (dataGridView1.AllowUserToAddRows == true)
+            {
+                rowCount = rowCount - 1;
+            }
+
+            //dgv内の重複を削除
+            for (int i = rowCount-1; i >=0; i--)
+            {
+                progressBar1.Value = (i * 100 + 1) / rowCount;
+                progressBar1.Refresh();
+                if (dataGridView1["list_dup",i].Value == "o")
+                {
+                    dataGridView1.Rows.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+}
